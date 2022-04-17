73027b5f224f312b5a2892256f6667daded13c84
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-04-17 15:09:54 +0200

    Add function, and add classes.

---
+++ main.js
@@ -0,0 +1,12 @@
+const apostles = document. querySelectorAll(".a-none")
+const showApostles = document.querySelector(".more")
+
+showApostles.addEventListener("click", () => {
+showApostles.classList.add('r-more')
+})
+showApostles.addEventListener("click", () => {
+    apostles.forEach((element) => {
+        element.style.display = "block"
+    })
+})
+
