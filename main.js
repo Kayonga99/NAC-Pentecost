fbe4d2a52f9bfd922a095c6f88bbb8151092ca3a
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-04-20 17:40:57 +0200

    Add div, add class and an id, and add function.

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const apostles = document.querySelectorAll('.a-none');
 const showApostles = document.querySelector('.more');
+const openMenu = document.querySelectorAll('.menu-tog')
+const closeMenu = document.querySelector('.close-x');
+const aboutMenu = document.getElementById('menu')
 
 showApostles.addEventListener('click', () => {
   showApostles.classList.add('r-more');
@@ -9,3 +12,13 @@ showApostles.addEventListener('click', () => {
     element.style.display = 'block';
   });
 });
+
+openMenu.forEach((element) => {
+element.addEventListener('click', () => {
+  aboutMenu.style.display = 'block';
+})
+})
+
+aboutMenu.addEventListener('click', () => {
+  aboutMenu.style.display = 'none';
+})
